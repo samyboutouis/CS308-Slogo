1c430b7214c3c19d6ce0315b5261e11c374da58f
Felix Jiang <felix.jiang@duke.edu>
2021-03-02 21:25:24 -0500

    visualization files done

---
+++ doc/plan/api/visualization/Display.java
@@ -0,0 +1,36 @@
+package slogo.visualization;
+
+public interface Display {
+  public void addToHistory(String input);
+  // adds what is given as input to the history part of the Display
+
+  public void showError(String error);
+  // display error message to a separate window
+
+  public void updateVariable(Map<String, Integer>);
+  // update variable as defined by user and update the display accordingly
+
+  public String receivePenColor();
+  // Get the hex code of the color the user types in the UI
+
+  public String receiveBackgroundColor();
+  // Get the hex code of the background color the user types in the UI
+
+  public void changePenColor(Color color);
+  // Sets the turtle pen color to the inputted color
+
+  public void changeBackgroundColor(Color color);
+  // Sets the background color to the inputted color
+
+  public Image getImage(FileChooser fileChooser);
+  // gets the image selected from a file FileChooser
+
+  public void setImage(Image image);
+  // sets the image of the turtle to the image passed in
+
+  public void openReference();
+  // opens the reference page with help
+
+  public void clearScreen();
+  // resets the turtle position and clears user created variables and history
+}
---
+++ doc/plan/api/visualization/Turtle.java
@@ -0,0 +1,20 @@
+package slogo.visualization;
+
+public interface Turtle {
+  /*
+   * This method, given a list of movements created by the back-end from a user's input will cause the turtle to continually update to visualize all the movements. Will be called once the back-end parses a user's command.
+   */
+  public void display(MovementList movements);
+
+  /*
+   * This method will update the turtle's position based on a movement passed in from the movement list. Based on the specific movement passed in, including a turtle's new position, orientation, and
+   pen status, the turtle on the screen will proceed to
+   move, rotate, and/or draw on the screen.
+   */
+  public void updateTurtle(Movement movement);
+
+  /*
+   * This method will create a movement object based on the turtle's current state. Specifically, it will create the new movement object by getting all the important variables of the turtle including position,rotatation, and pen status. This movement object will be passed to the back-end to calculate the new movement position.
+   */
+  public Movement createMovement(double turtleX, double turtleY, double turtleRotation, boolean isPenDown);
+}
