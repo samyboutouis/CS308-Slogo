ea9f8368fe120102e8524ee0b7af57586522969e
Felix Jiang <felix.jiang@duke.edu>
2021-03-02 21:24:12 -0500

    top level API files

---
+++ doc/plan/api/MovementList.java
@@ -0,0 +1,10 @@
+public class MovementList {
+  List<Movement> list;
+
+  public MovementList(){
+    list = new ArrayList<>();
+  }
+
+  // so code would compile
+  // actual implementation details would include pretty standard list commands
+}
