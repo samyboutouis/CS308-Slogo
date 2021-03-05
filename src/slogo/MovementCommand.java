07bf40844246cd1212074a03330a90c390835461
Felix Jiang <felix.jiang@duke.edu>
2021-03-05 00:59:01 -0500

    start movement command and turtle

---
+++ src/slogo/MovementCommand.java
@@ -0,0 +1,13 @@
+package slogo;
+
+public class MovementCommand implements Command{
+
+  public MovementCommand(double forward, double directionChange){
+
+  }
+
+  @Override
+  public void doCommand(Turtle turtle) {
+
+  }
+}
--- src/slogo/Turtle.java
+++ src/slogo/Turtle.java
@@ -1,7 +1,16 @@
 package slogo;
 
 public class Turtle {
+  private double xCor;
+  private double yCor;
+  private double direction;
+
   public Turtle(){
 
   }
+
+  public void forward(double pixels){
+
+  }
+
 }
