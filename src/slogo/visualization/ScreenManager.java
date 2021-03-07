e9eaabd0c83d6c6af914d8c55c148f87ca17688b
Samy Boutouis <sb590@duke.edu>
2021-03-07 13:56:07 -0500

    Reformat screen manager code

--- src/slogo/visualization/ScreenManager.java
+++ src/slogo/visualization/ScreenManager.java
@@ -20,42 +20,39 @@ public class ScreenManager {
   private final Scene scene;
   private final GridPane gridPane;
 
-  public ScreenManager(Pane root, Scene scene){
+  public ScreenManager(Pane root, Scene scene) {
     this.scene = scene;
-
     gridPane = new GridPane();
     root.getChildren().add(gridPane);
-
     setupGrid();
     setupDisplays();
     setStyleSheet();
   }
 
-  private void setupGrid(){
+  private void setupGrid() {
     initializeGridSize();
     initializeGridResizeListeners();
     initializeGridRowsAndCols();
   }
 
-  private void initializeGridSize(){
+  private void initializeGridSize() {
     gridPane.setMinSize(scene.getWidth(), scene.getHeight());
     gridPane.setVgap(PADDING_LENGTH);
     gridPane.setHgap(PADDING_LENGTH);
     gridPane.setPadding(new Insets(PADDING_LENGTH, PADDING_LENGTH, PADDING_LENGTH, PADDING_LENGTH));
   }
 
-  private void initializeGridResizeListeners(){
+  private void initializeGridResizeListeners() {
     scene.heightProperty().addListener((observableValue, oldHeight, newHeight) -> {
       gridPane.setMinHeight(newHeight.doubleValue());
     });
-
     scene.widthProperty().addListener((observableValue, oldWidth, newWidth) -> {
       gridPane.setMinWidth(newWidth.doubleValue());
     });
   }
 
-  private void initializeGridRowsAndCols(){
-    for(int i = 0; i < GRID_LENGTH; i++){
+  private void initializeGridRowsAndCols() {
+    for (int i = 0; i < GRID_LENGTH; i++) {
       RowConstraints row = new RowConstraints();
       ColumnConstraints col = new ColumnConstraints();
       row.setVgrow(Priority.ALWAYS);
@@ -67,36 +64,36 @@ public class ScreenManager {
     }
   }
 
-  private void setupDisplays(){
+  private void setupDisplays() {
     Pane turtlePane = new Pane();
     GridPane terminalPane = new GridPane();
     GridPane historyPane = new GridPane();
     VBox variablesPane = new VBox();
     VBox userCommandsPane = new VBox();
     HBox toolbarPane = new HBox();
-
     turtlePane.getStyleClass().add(DISPLAY_CLASS_NAME);
     terminalPane.getStyleClass().add(DISPLAY_CLASS_NAME);
     historyPane.getStyleClass().add(DISPLAY_CLASS_NAME);
     variablesPane.getStyleClass().add(DISPLAY_CLASS_NAME);
     userCommandsPane.getStyleClass().add(DISPLAY_CLASS_NAME);
     toolbarPane.getStyleClass().add(DISPLAY_CLASS_NAME);
-
     gridPane.add(toolbarPane, 0, 0, 10, 1);
     gridPane.add(turtlePane, 0, 1, 5, 7);
     gridPane.add(terminalPane, 0, 8, 5, 2);
     gridPane.add(historyPane, 5, 1, 5, 5);
     gridPane.add(variablesPane, 5, 6, 2, 4);
     gridPane.add(userCommandsPane, 7, 6, 3, 4);
-
     new TurtleDisplay(turtlePane);
-    new TerminalDisplay(terminalPane, RESOURCE_PACKAGE, new HistoryDisplay(historyPane, RESOURCE_PACKAGE));
+    new TerminalDisplay(terminalPane, RESOURCE_PACKAGE,
+      new HistoryDisplay(historyPane, RESOURCE_PACKAGE));
     new VariablesDisplay(variablesPane);
     new UserCommandsDisplay(userCommandsPane);
     new ToolbarDisplay(toolbarPane);
   }
 
-  private void setStyleSheet(){
-    scene.getStylesheets().add(getClass().getResource(String.format("/%s/stylesheets/%s", RESOURCE_PACKAGE, "default.css")).toExternalForm());
+  private void setStyleSheet() {
+    scene.getStylesheets().add(
+      getClass().getResource(String.format("/%s/stylesheets/%s", RESOURCE_PACKAGE, "default.css"))
+        .toExternalForm());
   }
 }
