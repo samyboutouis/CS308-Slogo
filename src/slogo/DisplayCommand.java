dea8cebbe29935311904b84424ed4050301f10bf
Felix Jiang <felix.jiang@duke.edu>
2021-03-09 12:26:58 -0500

    create command for displayin queries

---
+++ src/slogo/DisplayCommand.java
@@ -0,0 +1,14 @@
+package slogo;
+
+// used to display information from backend to frontend
+public class DisplayCommand implements Command{
+
+  public DisplayCommand(String info){
+    
+  }
+
+  @Override
+  public void doCommand(Turtle turtle) {
+
+  }
+}
