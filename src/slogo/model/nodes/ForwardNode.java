0c9b3f793788f2eddad87e902c88aa4285a6b567
Felix Jiang <felix.jiang@duke.edu>
2021-03-12 23:46:58 -0500

    slogo node method is public and forwardNode done

--- src/slogo/model/nodes/ForwardNode.java
+++ src/slogo/model/nodes/ForwardNode.java
@@ -1,2 +1,38 @@
-package slogo.model.nodes;public class ForwardNode {
+package slogo.model.nodes;
+
+import java.util.ArrayList;
+import java.util.List;
+import slogo.Command;
+import slogo.MovementCommand;
+import slogo.model.SlogoNode;
+
+public class ForwardNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+  private List<Double> values;
+
+  public ForwardNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+    values = new ArrayList<>();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    getValues(commands);
+    createMovement(commands);
+    return values.get(0); // only one value for a forward node
+  }
+
+  private void createMovement(List<Command> commands) {
+    commands.add(new MovementCommand(values.get(0), 0));
+  }
+
+  // gets values for all parameters of this node, needs commands list to create the commands when
+  // the parameters call getReturnValue
+  private void getValues(List<Command> commands) {
+    for(SlogoNode node : parameters) {
+      values.add(node.getReturnValue(commands));
+    }
+  }
 }
