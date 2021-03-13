90c074be1248b10a68d14aeb255588bb1bc9e678
Felix Jiang <felix.jiang@duke.edu>
2021-03-13 14:54:34 -0500

    create test for checking if home command object is correctly created

--- test/slogo/model/CommandReaderTest.java
+++ test/slogo/model/CommandReaderTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import slogo.turtlecommands.HomeCommand;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommandReaderTest {
@@ -14,10 +16,17 @@ public class CommandReaderTest {
     myReader = new CommandReader("English");
   }
 
+  // test that values are correct based on Logo code
   @Test
   void testIf () {
     List<Double> correct = new ArrayList<>();
     correct.add(0.0);
     assertEquals(correct, myReader.testParseInput("if 0 [ if fd 0 [ fd 20 ] sum 50 50 ]"));
+  }
+
+  // test that command objects created are correct
+  @Test
+  void testHomeCommand () {
+    assertTrue(myReader.parseInput("home").get(0) instanceof HomeCommand);
   }
 }
