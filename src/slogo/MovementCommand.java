45894c039d362ee28e5047ea3d2d4fddaf496d43
Felix Jiang <felix.jiang@duke.edu>
2021-03-13 00:09:30 -0500

    finish movement command

--- src/slogo/MovementCommand.java
+++ src/slogo/MovementCommand.java
@@ -1,14 +1,19 @@
 package slogo;
 
 public class MovementCommand implements Command{
+  private double forward;
+  private double directionChange;
 
+  // forward and directionChange are relative values
   public MovementCommand(double forward, double directionChange){
-
+    this.forward = forward;
+    this.directionChange = directionChange;
   }
 
   @Override
   public void doCommand(Turtle turtle) {
-
+    turtle.forward(forward);
+    turtle.rotate(directionChange);
   }
 }
 
