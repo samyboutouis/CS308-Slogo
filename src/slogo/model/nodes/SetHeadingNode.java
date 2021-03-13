129fc6099066953d93c5a835b1bb7e25b85beac8
Felix Jiang <felix.jiang@duke.edu>
2021-03-13 00:41:36 -0500

    finish set heading

---
+++ src/slogo/model/nodes/SetHeadingNode.java
@@ -0,0 +1,36 @@
+package slogo.model.nodes;
+
+import java.util.ArrayList;
+import java.util.List;
+import slogo.Command;
+import slogo.DegreeDirectionCommand;
+import slogo.model.SlogoNode;
+
+public class SetHeadingNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+  private List<Double> values;
+
+  public SetHeadingNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+    values = new ArrayList<>();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    getValues(commands);
+    createMovement(commands);
+    return values.get(0);
+  }
+
+  private void createMovement(List<Command> commands) {
+    commands.add(new DegreeDirectionCommand(values.get(0)));
+  }
+
+  private void getValues(List<Command> commands) {
+    for(SlogoNode node : parameters) {
+      values.add(node.getReturnValue(commands));
+    }
+  }
+}
