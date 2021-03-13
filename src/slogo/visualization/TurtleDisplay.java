1fe66a7f83fbaa124b03457eb2e7807bbef7ef0d
Samy Boutouis <sb590@duke.edu>
2021-03-13 00:12:21 -0500

    Add test buttons

--- src/slogo/visualization/TurtleDisplay.java
+++ src/slogo/visualization/TurtleDisplay.java
@@ -2,10 +2,12 @@ package slogo.visualization;
 
 import java.io.File;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import slogo.Turtle;
 
@@ -19,7 +21,39 @@ public class TurtleDisplay {
   public TurtleDisplay(AnchorPane anchorPane) {
     this.anchorPane = anchorPane;
     this.turtle = new Turtle(anchorPane);
+    setScreen();
   }
+
+  private void setScreen() {
+    testButtons();
+  }
+
+  private void testButtons() {
+    Button button = new Button("Right");
+    button.setOnAction(event -> turtle.rotate( 10));
+    Button left = new Button("Left");
+    left.setOnAction(event -> turtle.rotate( -10));
+    Button forward = new Button("Forward");
+    forward.setOnAction(event -> turtle.forward( 10));
+    Button back = new Button("Back");
+    back.setOnAction(event -> turtle.back( 10));
+    Button penUp = new Button("Pen Up");
+    penUp.setOnAction(event -> turtle.penUp());
+    Button penDown = new Button("Pen Down");
+    penDown.setOnAction(event -> turtle.penDown());
+    Button clear = new Button("Clear");
+    clear.setOnAction(event -> turtle.clearScreen());
+    VBox vBox = new VBox();
+    vBox.getChildren().add(button);
+    vBox.getChildren().add(left);
+    vBox.getChildren().add(forward);
+    vBox.getChildren().add(back);
+    vBox.getChildren().add(penUp);
+    vBox.getChildren().add(penDown);
+    vBox.getChildren().add(clear);
+    anchorPane.getChildren().add(vBox);
+  }
+
 
   public void setTurtleImage(File file) {
     turtle.setImage(file);
