c30d3ac9c275f3693cb1facd29c5c0c21bbbb59e
Felix Jiang <felix.jiang@duke.edu>
2021-03-13 00:53:09 -0500

    finish make variable node and add its number of parameters to commands.properties

--- src/slogo/model/nodes/MakeVariableNode.java
+++ src/slogo/model/nodes/MakeVariableNode.java
@@ -1,23 +1,23 @@
-package slogo.model;
+package slogo.model.nodes;
 
 import java.util.List;
 import java.util.Map;
 import slogo.Command;
-import slogo.model.nodes.VariableNode;
+import slogo.model.SlogoNode;
 
 // implements make and set commands
-public class AssignNode extends SlogoNode{
+public class MakeVariableNode extends SlogoNode {
   private List<SlogoNode> parameters;
   private Map<String, Double> variables;
 
-  public AssignNode(String type, Map<String, Double> variables){
-    super(2, type);
+  public MakeVariableNode(int numParameters, Map<String, Double> variables){
+    super(numParameters);
     parameters = super.getParameters();
     this.variables = variables;
   }
 
   @Override
-  protected double getReturnValue(List<Command> commands) {
+  public double getReturnValue(List<Command> commands) {
     double value = parameters.get(1).getReturnValue(commands);
     SlogoNode leftChild = parameters.get(0);
     if(leftChild instanceof VariableNode){
