b924be994a166ac35326bcfc97508ec96b4afe4f
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 15:03:17 -0400

    implement left node

---
+++ src/slogo/model/nodes/commands/LeftNode.java
@@ -0,0 +1,39 @@
+package slogo.model.nodes.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+import slogo.Command;
+import slogo.turtlecommands.MovementCommand;
+import slogo.model.SlogoNode;
+
+public class LeftNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+  private List<Double> values;
+
+  public LeftNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+    values = new ArrayList<>();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    getValues(commands);
+    createMovement(commands);
+    return values.get(0); // only one value for a Left node
+  }
+
+  private void createMovement(List<Command> commands) {
+    commands.add(new MovementCommand(0, -1 * values.get(0)));
+    // move Left the amount in values.get(0)
+  }
+
+  // gets values for all parameters of this node, needs commands list to create the commands when
+  // the parameters call getReturnValue
+  private void getValues(List<Command> commands) {
+    for(SlogoNode node : parameters) {
+      values.add(node.getReturnValue(commands));
+    }
+  }
+}
