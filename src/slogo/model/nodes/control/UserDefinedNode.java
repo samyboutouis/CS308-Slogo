15a54efe73f720bd8343d8f58975aba1ae2de586
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 17:00:36 -0400

    start user defined

---
+++ src/slogo/model/nodes/control/UserDefinedNode.java
@@ -0,0 +1,6 @@
+package slogo.model.nodes.control;
+
+// represents the actual node when a user calls the command they defined, so return value is not just 1 or 0
+public class UserDefinedNode {
+
+}
