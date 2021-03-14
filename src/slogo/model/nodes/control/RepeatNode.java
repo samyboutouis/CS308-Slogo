792581f3eb3693f8b179bf633b30603e7dea6e4d
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 15:48:23 -0400

    finish repeat node

--- src/slogo/model/nodes/control/RepeatNode.java
+++ src/slogo/model/nodes/control/RepeatNode.java
@@ -10,19 +10,17 @@ public class RepeatNode extends SlogoNode {
   private List<SlogoNode> parameters;
   private Map<String, Double> variables;
   private VariableNode variable;
-  private int brackets;
   private double repeatEnd;
 
   public RepeatNode(int numParameters, Map<String, Double> variables) {
-    super(numParameters); // parameters being full determined by bracket, just like conditional node, so this is a dummy value
-    brackets = numParameters;
+    super(numParameters); // parameters being full determined like IfNode (last one is ListEnd)
     this.variables = variables;
     parameters = super.getParameters();
   }
 
   @Override
   public boolean isFull() {
-    return !parameters.isEmpty() && checkBrackets();
+    return !parameters.isEmpty() && parameters.get(parameters.size() - 1) instanceof ListEndNode;
   }
 
   @Override
@@ -47,16 +45,5 @@ public class RepeatNode extends SlogoNode {
     // i = 1 is left bracket
     variable = new VariableNode(0, variables, ":repcount");
     repeatEnd = parameters.get(0).getReturnValue(commands);
-  }
-
-  // check to see if we've seen brackets number of list end nodes
-  private boolean checkBrackets() {
-    int seen = 0;
-    for(SlogoNode node : parameters){
-      if(node instanceof ListEndNode){
-        seen++;
-      }
-    }
-    return seen == brackets;
   }
 }
