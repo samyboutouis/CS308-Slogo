559e42a58268b3610b7db9dfcee5184d5fd16d72
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 15:20:58 -0400

    and node

---
+++ src/slogo/model/nodes/booleans/AndNode.java
@@ -0,0 +1,22 @@
+package slogo.model.nodes.booleans;
+
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
+
+public class AndNode extends SlogoNode{
+  private List<SlogoNode> parameters;
+
+  public AndNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    if(parameters.get(0).getReturnValue(commands) != 0.0 && parameters.get(1).getReturnValue(commands) != 0.0){
+      return 1;
+    }
+    return 0;
+  }
+}
