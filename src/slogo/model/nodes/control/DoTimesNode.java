5b9e7b79e6b00cc5113d745e833d6e5c9ff62c30
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 15:55:05 -0400

    dotimes

--- src/slogo/model/nodes/control/DoTimesNode.java
+++ src/slogo/model/nodes/control/DoTimesNode.java
@@ -1,2 +1,69 @@
-package slogo.model.nodes.control;public class DoTimesNode {
+package slogo.model.nodes.control;
+
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
+
+public class DoTimesNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+  private VariableNode variable;
+  private int brackets;
+  private double doTimesEnd;
+  private int firstEnd;
+
+  public DoTimesNode(int numParameters) {
+    super(numParameters); // parameters being full determined by bracket, just like conditional node, so this is a dummy value
+    brackets = numParameters;
+    parameters = super.getParameters();
+  }
+
+  @Override
+  public boolean isFull() {
+    return !parameters.isEmpty() && checkBrackets();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    setFirstEnd();
+    getIndexing(commands);
+    double ret = 0;
+    for(double i = 1; i <= doTimesEnd; i++){
+      variable.setValue(i);
+      for(int j = firstEnd; j < parameters.size(); j++){ // runs through all the commands in the loop
+        if(!(parameters.get(j) instanceof ListStartNode) && !(parameters.get(j) instanceof ListEndNode)){
+          ret = parameters.get(j).getReturnValue(commands); // ret is value of last command executed
+        }
+      }
+    }
+    return ret;
+  }
+
+  // get limit
+  private void getIndexing(List<Command> commands){
+    // dotimes [ :a fd 50 ] [ fd 50 ]
+    // i = 0 is left bracket
+    variable = (VariableNode) parameters.get(1);
+    doTimesEnd = parameters.get(2).getReturnValue(commands);
+  }
+
+  private void setFirstEnd() {
+    for(int i = 0; i < parameters.size(); i++){
+      if(parameters.get(i) instanceof ListEndNode){
+        firstEnd = i;
+        break;
+      }
+    }
+  }
+
+  // check to see if we've seen brackets number of list end nodes
+  private boolean checkBrackets() {
+    int seen = 0;
+    for(SlogoNode node : parameters){
+      if(node instanceof ListEndNode){
+        seen++;
+      }
+    }
+    return seen == brackets;
+  }
 }
