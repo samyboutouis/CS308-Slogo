51ec913c78c3666fcd63321e9b31499f404c7c02
Andre Wang <jw542@duke.edu>
2021-03-15 10:10:03 +0800

    cosine node

--- src/slogo/model/nodes/math/CosineNode.java
+++ src/slogo/model/nodes/math/CosineNode.java
@@ -1,5 +1,20 @@
 package slogo.model.nodes.math;
 
-public class CosineNode {
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
 
+public class CosineNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+
+  public CosineNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    return Math.cos(parameters.get(0).getReturnValue(commands));
+  }
 }
