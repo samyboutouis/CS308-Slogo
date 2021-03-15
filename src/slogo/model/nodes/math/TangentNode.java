7e44f8bc10b474ea962cb7822e22a41476897d7f
Andre Wang <jw542@duke.edu>
2021-03-15 10:10:37 +0800

    cosine node

--- src/slogo/model/nodes/math/TangentNode.java
+++ src/slogo/model/nodes/math/TangentNode.java
@@ -1,5 +1,21 @@
 package slogo.model.nodes.math;
 
-public class TangentNode {
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
 
+public class TangentNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+
+  public TangentNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    return Math.tan(parameters.get(0).getReturnValue(commands));
+  }
 }
+
