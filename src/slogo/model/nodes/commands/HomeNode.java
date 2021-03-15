aa9f9124d074de5a5b3495748f70a19ffd410e78
Felix Jiang <felix.jiang@duke.edu>
2021-03-14 23:56:20 -0400

    finish home node

--- src/slogo/model/nodes/commands/HomeNode.java
+++ src/slogo/model/nodes/commands/HomeNode.java
@@ -1,19 +1,26 @@
 package slogo.model.nodes.commands;
 
 import java.util.List;
+import slogo.BackEndTurtle;
 import slogo.Command;
 import slogo.model.SlogoNode;
 import slogo.turtlecommands.HomeCommand;
 
 public class HomeNode extends SlogoNode {
+  private BackEndTurtle turtle;
 
-  public HomeNode(int numParameters) {
+  public HomeNode(int numParameters, BackEndTurtle turtle) {
     super(numParameters);
+    this.turtle = turtle;
   }
 
   @Override
   public double getReturnValue(List<Command> commands) {
     commands.add(new HomeCommand());
-    return 0;
+    double prevX = turtle.getX();
+    double prevY = turtle.getY();
+    turtle.home();
+    return Math.sqrt(Math.pow(prevX - turtle.getX(), 2) +  Math.pow(prevY - turtle.getY(), 2));
+    // fd fd 50 fd 50 home
   }
 }
