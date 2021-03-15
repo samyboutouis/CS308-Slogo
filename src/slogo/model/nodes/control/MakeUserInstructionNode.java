4771a082973a047d220f8ce39621e4399e37a83e
Felix Jiang <felix.jiang@duke.edu>
2021-03-15 01:24:14 -0400

    finish make user instruction

--- src/slogo/model/nodes/control/MakeUserInstructionNode.java
+++ src/slogo/model/nodes/control/MakeUserInstructionNode.java
@@ -1,13 +1,16 @@
 package slogo.model.nodes.control;
 
 import java.util.List;
+import java.util.Map;
 import slogo.Command;
 import slogo.model.SlogoNode;
 
 public class MakeUserInstructionNode extends SlogoNode {
 
   private List<SlogoNode> parameters;
-  private int brackets; // how many pairs of brackets to expect
+  private int brackets;
+  private int firstEnd;
+  private UserDefinedNode definedNode;
 
   // this node creates the user defined node and adds it to the map of commands
   // that node needs a list of string to represent the variable names
@@ -25,28 +28,31 @@ public class MakeUserInstructionNode extends SlogoNode {
 
   @Override
   public double getReturnValue(List<Command> commands) {
-    int firstEnd = getFirstEnd(); // index of first end bracket
-    int start = 0;
-    int end = 0;
-    double ret = 0;
-    if(parameters.get(0).getReturnValue(commands) != 0.0) { // does the expression part
-      start = 1; // i = 1 should be list start (hop over expr)
-      end = firstEnd;
-    }
-    else{
-      start = firstEnd;
-      end = parameters.size();
-    }
-    for(int i = start; i < end; i++){
-      if(!(parameters.get(i) instanceof ListStartNode) && !(parameters.get(i) instanceof ListEndNode)){
-        ret = parameters.get(i).getReturnValue(commands); // ret is value of last command executed
+    try{
+      getFirstEnd(); // index of first end bracket
+      getVariables();
+      for(int i = firstEnd; i < parameters.size(); i++){ // technically could do i = firstEnd + 2 but our code handles i = firstEnd
+        if(!(parameters.get(i) instanceof ListStartNode) && !(parameters.get(i) instanceof ListEndNode)){
+          definedNode.addCommand(parameters.get(i));
+        }
       }
+      return 1;
+    } catch (ClassCastException c) {
+      // if name wasn't a user defined node (already exists)
+      return 0;
     }
-    return 1;
+  }
+
+  private void getVariables() {
+    // to arc [ :incr :degrees ]
+    // to arc [ ]
+    definedNode = (UserDefinedNode) parameters.get(0);
+    for(int i = 2; i < firstEnd; i++){
+      definedNode.addVariableName((VariableNode) parameters.get(i));
+    }
   }
 
   private int getFirstEnd() {
-    int firstEnd = 0;
     for(int i = 0; i < parameters.size(); i++){
       if(parameters.get(i) instanceof ListEndNode){
         firstEnd = i;
