e44182e646a7aa1bffb1d4adb03a915a941a8dcd
Felix Jiang <felix.jiang@duke.edu>
2021-03-15 00:28:00 -0400

    user defined node

--- src/slogo/model/nodes/control/UserDefinedNode.java
+++ src/slogo/model/nodes/control/UserDefinedNode.java
@@ -1,6 +1,25 @@
 package slogo.model.nodes.control;
 
-// represents the actual node when a user calls the command they defined, so return value is not just 1 or 0
-public class UserDefinedNode {
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
 
+// represents the actual node when a user calls the command they defined, so return value is not just 1 or 0
+public class UserDefinedNode extends SlogoNode {
+
+  private List<SlogoNode> myCommands;
+
+  public UserDefinedNode(int numParameters, List<SlogoNode> myCommands) {
+    super(numParameters);
+    this.myCommands = myCommands;
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    double ret = 0;
+    for(SlogoNode node : myCommands) {
+      ret = node.getReturnValue(commands);
+    }
+    return ret;
+  }
 }
