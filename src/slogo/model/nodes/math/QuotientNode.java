32c3d77bb5e595ccf34f6c8f8ecf17bb079b6395
Andre Wang <jw542@duke.edu>
2021-03-15 10:01:41 +0800

    quotient node

---
+++ src/slogo/model/nodes/math/QuotientNode.java
@@ -0,0 +1,22 @@
+package slogo.model.nodes.math;
+
+
+import java.util.List;
+import slogo.Command;
+import slogo.model.SlogoNode;
+
+public class QuotientNode extends SlogoNode {
+
+  private List<SlogoNode> parameters;
+
+  public QuotientNode(int numParameters){
+    super(numParameters);
+    parameters = super.getParameters();
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    return parameters.get(0).getReturnValue(commands) / parameters.get(1).getReturnValue(commands);
+  }
+}
+
