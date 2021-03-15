5fda26c8e6a67a3ff119888fdeb749e7ec6ecf63
Donghan Park <donghan.park@duke.edu>
2021-03-14 23:02:55 -0400

    added timeline animation

--- src/slogo/visualization/AnimationManager.java
+++ src/slogo/visualization/AnimationManager.java
@@ -1,6 +1,10 @@
 package slogo.visualization;
 
+import java.sql.Time;
 import java.util.List;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 import slogo.Command;
 import slogo.Turtle;
 
@@ -9,16 +13,36 @@ public class AnimationManager {
   private final List<Command> commands;
   private final Turtle turtle;
 
+  private Timeline animation;
+
   public AnimationManager(List<Command> commands, Turtle turtle){
     this.commands = commands;
     this.turtle = turtle;
 
-    playAnimation();
+    setupTimeline();
   }
 
-  private void playAnimation(){
-    for(Command c : commands) {
-      c.doCommand(turtle);
+  private void setupTimeline(){
+    int FPS = 10;
+    double secondDelay = 1.0 / FPS;
+
+    int framesCount = 0;
+
+    KeyFrame keyframe = new KeyFrame(Duration.seconds(secondDelay), e -> {
+      stepAnimation(framesCount);
+    });
+
+    animation = new Timeline();
+    animation.setCycleCount(Timeline.INDEFINITE);
+    animation.getKeyFrames().add(keyframe);
+    animation.play();
+  }
+
+  private void stepAnimation(int index){
+    commands.get(index).doCommand(turtle);
+    index++;
+    if (index >= commands.size()){
+      animation.stop();
     }
   }
 }
