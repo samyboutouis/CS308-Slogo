22aafb6b7a42819a99b6d8fd65a9309d2e9626de
Felix Jiang <felix.jiang@duke.edu>
2021-03-17 19:38:51 -0400

    no constructor takes in a turtle object'

--- src/slogo/model/CommandReader.java
+++ src/slogo/model/CommandReader.java
@@ -139,15 +139,8 @@ public class CommandReader {
         case "MakeUserInstruction" -> {
           curr = new MakeUserInstructionNode(parameters);
         }
-
         default -> {
-          try{
-            curr = (SlogoNode) node.getDeclaredConstructor(Integer.TYPE, tracker.getTurtle(tracker.getCurr()).getClass()).newInstance(parameters);
-          } catch(NoSuchMethodException e) {
-            // constructor of this type doesn't exist
-            //System.out.println("Felix: method doesn't exist");
-            curr = (SlogoNode) node.getDeclaredConstructor(Integer.TYPE).newInstance(parameters);
-          }
+          curr = (SlogoNode) node.getDeclaredConstructor(Integer.TYPE).newInstance(parameters);
         }
       }
       if(curr.isFull()){ // only true if node has no parameters
