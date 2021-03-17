5a23dc91be41d67df34164ad0061c18446c6a622
Felix Jiang <felix.jiang@duke.edu>
2021-03-16 23:46:20 -0400

    right node takes in turtle

--- src/slogo/model/nodes/commands/RightNode.java
+++ src/slogo/model/nodes/commands/RightNode.java
@@ -1,24 +1,30 @@
 package slogo.model.nodes.commands;
 
 import java.util.List;
+import slogo.BackEndTurtle;
 import slogo.Command;
+import slogo.Turtle;
 import slogo.turtlecommands.MovementCommand;
 import slogo.model.SlogoNode;
 
 public class RightNode extends TurtleCommandNode {
 
+  private Turtle turtle;
+
   private List<SlogoNode> parameters;
   private List<Double> values;
 
-  public RightNode(int numParameters){
+  public RightNode(int numParameters, BackEndTurtle turtle){
     super(numParameters);
     parameters = super.getParameters();
+    this.turtle = turtle;
   }
 
   @Override
   public double getReturnValue(List<Command> commands) {
     values = super.getValues(commands, parameters);
     createMovement(commands);
+    turtle.rotate(values.get(0));
     return values.get(0); // only one value for a Right node
   }
 
