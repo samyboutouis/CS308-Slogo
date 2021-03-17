dd1e830c55f7d66e3e41e542ba12c081b0af7f93
Felix Jiang <felix.jiang@duke.edu>
2021-03-17 03:30:37 -0400

    functional interface for turtle commands

--- src/slogo/model/nodes/commands/TurtleAction.java
+++ src/slogo/model/nodes/commands/TurtleAction.java
@@ -1,2 +1,9 @@
-package slogo.model.nodes.commands;public interface TurtleAction {
+package slogo.model.nodes.commands;
+
+import java.util.List;
+import slogo.BackEndTurtle;
+
+@FunctionalInterface
+public interface TurtleAction {
+  double turtleAction(BackEndTurtle turtle, List<Double> values);
 }
