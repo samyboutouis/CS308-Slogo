cdd3ff9cd2ddc0e3698177df30789c4cfaf7e55c
Felix Jiang <felix.jiang@duke.edu>
2021-03-17 03:35:08 -0400

    Overall object to handle all values with turtles

--- src/slogo/model/TurtleTracker.java
+++ src/slogo/model/TurtleTracker.java
@@ -1,2 +1,57 @@
-package slogo.model;public class TurtleTracker {
+package slogo.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import slogo.BackEndTurtle;
+
+// manages all turtles in the backend,
+public class TurtleTracker {
+
+  // assume each new Tell overrides previous Tell
+  private Map<Integer, BackEndTurtle> allTurtles;
+  private List<Integer> activeTurtles;
+  private int currTurtle;
+
+  public TurtleTracker() {
+    allTurtles = new HashMap<>();
+    activeTurtles = new ArrayList<>();
+    currTurtle = 0;
+  }
+
+  // loops through active turtles for one node, will have many iterator instances out for nested commands e.g. fd fd 50
+  public Iterator<Integer> getIterator() {
+    // returns a new, different iterator each time
+    return activeTurtles.iterator();
+  }
+
+  public BackEndTurtle getTurtle(int index) {
+    // not possible for a turtle to not be in allTurtles when using iterator
+    return allTurtles.get(index);
+  }
+
+  // represents current turtle globally
+  public void setCurr(int index) {
+    currTurtle = index;
+  }
+
+  // helpful for ID method
+  public int getCurr() {
+    return currTurtle;
+    // tell [ 1 2 3 ] fd ID
+  }
+
+//  tell [ 1 2 3 ]
+//  set :x 10
+//  fd * id :x
+
+  // tell [ 1 2 ]
+  // 2 6, 3 12, 4 20
+  // fd fd 20
+  // order of things done: 1fd 21, 2fd 22, 1fd 22, fd
+
+
+
 }
