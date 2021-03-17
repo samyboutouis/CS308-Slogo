a82240cb0470395908cbaf385e71f071090c10d5
Felix Jiang <felix.jiang@duke.edu>
2021-03-16 22:40:31 -0400

    finish clear screen node

--- src/slogo/model/nodes/commands/ClearScreenNode.java
+++ src/slogo/model/nodes/commands/ClearScreenNode.java
@@ -1,5 +1,25 @@
 package slogo.model.nodes.commands;
 
-public class ClearScreenNode {
+import java.util.List;
+import slogo.Command;
+import slogo.Turtle;
+import slogo.model.SlogoNode;
+import slogo.turtlecommands.ClearScreenCommand;
 
+public class ClearScreenNode extends SlogoNode {
+  private Turtle turtle;
+
+  public ClearScreenNode(int numParameters, Turtle turtle){
+    super(numParameters);
+    this.turtle = turtle;
+  }
+
+  @Override
+  public double getReturnValue(List<Command> commands) {
+    commands.add(new ClearScreenCommand());
+    double prevX = turtle.getX();
+    double prevY = turtle.getY();
+    turtle.clearScreen();
+    return Math.sqrt(Math.pow(prevX - turtle.getX(), 2) +  Math.pow(prevY - turtle.getY(), 2));
+  }
 }
