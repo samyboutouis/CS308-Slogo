fff495f9ff41e89a0db9a3ae47708e507bbe4202
Felix Jiang <felix.jiang@duke.edu>
2021-03-16 22:47:59 -0400

    complete set heading node

--- src/slogo/model/nodes/commands/SetHeadingNode.java
+++ src/slogo/model/nodes/commands/SetHeadingNode.java
@@ -1,27 +1,31 @@
 package slogo.model.nodes.commands;
 
-import java.util.ArrayList;
 import java.util.List;
 import slogo.Command;
+import slogo.Turtle;
 import slogo.turtlecommands.SetHeadingCommand;
 import slogo.model.SlogoNode;
 
 public class SetHeadingNode extends TurtleCommandNode {
+  private Turtle turtle;
 
   private List<SlogoNode> parameters;
   private List<Double> values;
 
-  public SetHeadingNode(int numParameters){
+  public SetHeadingNode(int numParameters, Turtle turtle){
     super(numParameters);
     parameters = super.getParameters();
-    values = new ArrayList<>();
+    this.turtle = turtle;
   }
 
   @Override
   public double getReturnValue(List<Command> commands) {
     values = super.getValues(commands, parameters);
+    // only one value so not entirely necessary, could just do value = parameters.get(0).getReturnValue(commands)
     createMovement(commands);
-    return values.get(0);
+    double prevHeading = turtle.getDirection();
+    turtle.setDirection(values.get(0));
+    return Math.abs(prevHeading - turtle.getDirection());
   }
 
   private void createMovement(List<Command> commands) {
