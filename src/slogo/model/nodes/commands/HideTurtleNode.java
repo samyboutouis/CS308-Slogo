7a0d160d8da00dd6ab2ba310e063ed90bab47a63
Felix Jiang <felix.jiang@duke.edu>
2021-03-16 23:55:59 -0400

    can hide turtle

--- src/slogo/model/nodes/commands/HideTurtleNode.java
+++ src/slogo/model/nodes/commands/HideTurtleNode.java
@@ -1,18 +1,24 @@
 package slogo.model.nodes.commands;
 
 import java.util.List;
+import slogo.BackEndTurtle;
 import slogo.Command;
+import slogo.Turtle;
 import slogo.model.SlogoNode;
 import slogo.turtlecommands.HideTurtleCommand;
 
 public class HideTurtleNode extends SlogoNode {
+  private Turtle turtle;
 
-  public HideTurtleNode(int numParameters) {
+  public HideTurtleNode(int numParameters, BackEndTurtle turtle) {
     super(numParameters);
+    this.turtle = turtle;
   }
+
   @Override
   public double getReturnValue(List<Command> commands) {
     commands.add(new HideTurtleCommand());
+    turtle.hide();
     return 1;
   }
 }
