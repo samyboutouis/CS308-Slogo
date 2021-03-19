04a75bc20251fbcf456b209bf5d20dd985b4b603
Felix Jiang <felix.jiang@duke.edu>
2021-03-19 04:41:36 +0000

    Update SafeFrontEndTurtleTracker.java ideas on how to implement new commands with flexibility

--- src/slogo/SafeFrontEndTurtleTracker.java
+++ src/slogo/SafeFrontEndTurtleTracker.java
@@ -5,4 +5,6 @@ public interface SafeFrontEndTurtleTracker {
   void addTurtle(int id);
 
   // can probably but methods that change global color in here as well
+  // Actually since color changes can occur in the middle of a long command, those need to be ran at the correct times, so either turtle class should have access to changing colors and adding turtles, or we pass in a bigger object that does what each turtle does but also changes the state of display
+  // OR we could pass in this class into those Command interface classes when we construct them, then it will be called in sync and no other code will require any changes. 
 }
