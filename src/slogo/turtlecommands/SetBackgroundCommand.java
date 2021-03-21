118a160718943008a7ca6a8f6534ef01990b8478
Felix Jiang <felix.jiang@duke.edu>
2021-03-21 02:29:46 -0400

    set up set bg command

--- src/slogo/turtlecommands/SetBackgroundCommand.java
+++ src/slogo/turtlecommands/SetBackgroundCommand.java
@@ -1,12 +1,21 @@
 package slogo.turtlecommands;
 
 import slogo.Command;
+import slogo.SafeFrontEndTurtleTracker;
 import slogo.visualization.FrontEndTurtle;
 
 public class SetBackgroundCommand implements Command {
 
+  private SafeFrontEndTurtleTracker safe;
+  private int index;
+
+  public SetBackgroundCommand(SafeFrontEndTurtleTracker safe, int index) {
+    this.safe = safe;
+    this.index = index;
+  }
+
   @Override
   public void doCommand(FrontEndTurtle frontEndTurtle) {
-
+    // safe.setbg
   }
 }
