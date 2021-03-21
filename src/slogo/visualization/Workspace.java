94308090fe35df53198df1d0215b443e3e032360
Samy Boutouis <sb590@duke.edu>
2021-03-21 14:58:38 -0400

    Fix button display

--- src/slogo/visualization/Workspace.java
+++ src/slogo/visualization/Workspace.java
@@ -36,15 +36,13 @@ public class Workspace {
 
 
   private void setupDisplays() {
-
+    PaletteDisplay paletteDisplay = new PaletteDisplay(this, RESOURCE_PACKAGE);
+    FrontEndTurtleTracker frontEndTurtleTracker = new FrontEndTurtleTracker(paletteDisplay);
+    frontEndController = new FrontEndController(stage, frontEndTurtleTracker);
     HistoryDisplay historyDisplay = new HistoryDisplay(this, RESOURCE_PACKAGE);
     VariablesDisplay variablesDisplay = new VariablesDisplay(this, RESOURCE_PACKAGE);
     UserCommandsDisplay userCommandsDisplay = new UserCommandsDisplay(this, RESOURCE_PACKAGE);
-    PaletteDisplay paletteDisplay = new PaletteDisplay(this, RESOURCE_PACKAGE);
     ButtonDisplay buttonDisplay = new ButtonDisplay(frontEndController);
-
-    FrontEndTurtleTracker frontEndTurtleTracker = new FrontEndTurtleTracker(paletteDisplay);
-    frontEndController = new FrontEndController(stage, frontEndTurtleTracker);
 
     TurtleStateDisplay turtleStateDisplay = new TurtleStateDisplay(frontEndController,
         frontEndTurtleTracker);
