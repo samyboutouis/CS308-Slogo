650a940684293b1133db28b175b354dfc0c725cd
Felix Jiang <felix.jiang@duke.edu>
2021-03-21 02:06:17 -0400

    background needs parameters too

--- src/slogo/model/nodes/display/SetBackgroundNode.java
+++ src/slogo/model/nodes/display/SetBackgroundNode.java
@@ -1,18 +1,23 @@
 package slogo.model.nodes.display;
 
 import java.util.ArrayList;
+import java.util.List;
 import slogo.model.BackEndTurtleTracker;
+import slogo.model.SlogoNode;
 import slogo.model.nodes.commands.TurtleCommandNode;
 
 public class SetBackgroundNode extends TurtleCommandNode {
 
+  private List<SlogoNode> parameters;
+
   public SetBackgroundNode(int numParameters) {
     super(numParameters);
+    parameters = super.getParameters();
   }
 
   @Override
   public double getReturnValue(BackEndTurtleTracker tracker) {
-    return super.loopThroughTurtles(tracker, new ArrayList<>(), (currTurtle, values) -> {
+    return super.loopThroughTurtles(tracker, parameters, (currTurtle, values) -> {
       // create set background command here
       return values.get(0);
     });
