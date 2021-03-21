f5126cd6d5da3c4165101b32f4bedbc5145d6bd8
Samy Boutouis <sb590@duke.edu>
2021-03-20 22:33:41 -0400

    modify toolbar

--- src/slogo/visualization/ToolbarDisplay.java
+++ src/slogo/visualization/ToolbarDisplay.java
@@ -75,11 +75,11 @@ public class ToolbarDisplay {
     for(String label : buttonList) {
       gridPane.add(buttonFactory.createToggleButton(label), colIndex++, 0, 1, 1);
     }
-    addLanguageDropdown();
+    addLanguageDropdown(colIndex++);
     addHelpDropdown();
   }
 
-  private void addLanguageDropdown() {
+  private void addLanguageDropdown(int colIndex) {
     ComboBox<String> comboBox = new ComboBox<>();
     comboBox.setId(idBundle.getString("LanguageDropdown"));
     for (String language : languageBundle.keySet()) {
@@ -87,7 +87,7 @@ public class ToolbarDisplay {
     }
     comboBox.setValue(DEFAULT_LANGUAGE);
     comboBox.setOnAction(event -> handleLanguageClick(comboBox.getValue()));
-    gridPane.add(comboBox, 4, 0, 4, 1);
+    gridPane.add(comboBox, colIndex, 0, 4, 1);
   }
 
   private void addHelpDropdown() {
