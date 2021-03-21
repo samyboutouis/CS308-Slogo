bf837887735ad3891a5c330c0975229e1caf431c
Felix Jiang <felix.jiang@duke.edu>
2021-03-20 23:03:50 -0400

    all allCommands method to also be in safe tracker for testing

--- src/slogo/SafeBackEndTurtleTracker.java
+++ src/slogo/SafeBackEndTurtleTracker.java
@@ -5,5 +5,7 @@ import java.util.Map;
 
 public interface SafeBackEndTurtleTracker {
 
-  Map<Integer, List<Command>> getAllTurtleCommands();
+  Map<Integer, List<Command>> getAllTurtleCommands(); // for front end to get map
+
+  List<Command> getAllCommands(); // for easy testing purposes
 }
