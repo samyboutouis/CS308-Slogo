4874531625ef3651c0bd9c5ca34123d3a78d270b
Samy Boutouis <sb590@duke.edu>
2021-03-22 00:57:34 -0400

    Button display tests

--- test/slogo/visualization/TurtleStateDisplayTest.java
+++ test/slogo/visualization/TurtleStateDisplayTest.java
@@ -8,7 +8,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-import slogo.Main;
 import util.DukeApplicationTest;
 
 import static org.junit.jupiter.api.Assertions.*;
