c732984b659c48f46034ef6c088df7b0a6d9a754
Felix Jiang <felix.jiang@duke.edu>
2021-03-22 02:15:15 -0400

    implement variable scoping in user defined commands by only changing code in one file

--- src/slogo/model/nodes/control/CommandNode.java
+++ src/slogo/model/nodes/control/CommandNode.java
@@ -1,5 +1,6 @@
 package slogo.model.nodes.control;
 
+import java.util.ArrayList;
 import java.util.List;
 import slogo.model.SlogoNode;
 import slogo.model.BackEndTurtleTracker;
@@ -10,12 +11,14 @@ public class CommandNode extends SlogoNode {
   private List<SlogoNode> myCommands;
   private List<SlogoNode> parameters;
   private List<VariableNode> variables;
+  private List<Double> prevValues;
 
   public CommandNode(int numParameters, List<SlogoNode> myCommands, List<VariableNode> variables) {
     super(numParameters);
     parameters = super.getParameters();
     this.myCommands = myCommands;
     this.variables = variables;
+    prevValues = new ArrayList<>();
   }
 
   @Override
@@ -25,13 +28,23 @@ public class CommandNode extends SlogoNode {
     for (SlogoNode node : myCommands) {
       ret = node.getReturnValue(tracker);
     }
+    updateVariableScope();
     return ret;
   }
 
   // size of parameters must be equal to size of variables
   private void setParameters(BackEndTurtleTracker tracker) {
     for (int i = 0; i < variables.size(); i++) {
+      // keep track of previous values once this command returns
+      prevValues.add(variables.get(i).getReturnValue(tracker));
       variables.get(i).setValue(parameters.get(i).getReturnValue(tracker));
+    }
+  }
+
+  private void updateVariableScope() {
+    for (int i = 0; i < variables.size(); i++) {
+      // if variable was undefined before, it's just 0 since it wasn't in the map
+      variables.get(i).setValue(prevValues.get(i));
     }
   }
 }
