30557ff6a4fb5bccce718cd8cee2e8f454cafba3
Felix Jiang <felix.jiang@duke.edu>
2021-03-21 22:17:27 -0400

    remove some uncommented out code

--- src/slogo/model/nodes/commands/LeftNode.java
+++ src/slogo/model/nodes/commands/LeftNode.java
@@ -14,19 +14,6 @@ public class LeftNode extends TurtleCommandNode {
     super(numParameters);
     parameters = super.getParameters();
   }
-/*
-  @Override
-  public double getReturnValue(List<Command> commands) {
-    values = super.getValues(commands, parameters);
-    createMovement(commands);
-    turtle.rotate((-1 * values.get(0)));
-    return values.get(0); // only one value for a Left node
-  }
-
-  private void createMovement(List<Command> commands) {
-    commands.add(new MovementCommand(0, -1 * values.get(0)));
-    // move Left the amount in values.get(0)
-  }*/
 
   @Override
   public double getReturnValue(BackEndTurtleTracker tracker) {
--- src/slogo/model/nodes/commands/TurtleCommandNode.java
+++ src/slogo/model/nodes/commands/TurtleCommandNode.java
@@ -35,16 +35,4 @@ public abstract class TurtleCommandNode extends SlogoNode {
     }
     return values;
   }
-
-  // OLD getValues Method
-  // for subclasses to get their parameter values
-  // gets values for all parameters of node that calls this method, needs commands list to
-  // create the commands when the parameters call getReturnValue
-//  protected List<Double> getValues(List<Command> commands, List<SlogoNode> parameters) {
-//    List<Double> values = new ArrayList<>();
-//    for(SlogoNode node : parameters) {
-//      values.add(node.getReturnValue(commands));
-//    }
-//    return values;
-//  }
 }
