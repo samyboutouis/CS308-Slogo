57abfe9cdc3a0328ea129054257b2fa1ba659d57
Donghan Park <donghan.park@duke.edu>
2021-03-22 01:30:50 -0400

    started fix for variables display test;

--- test/slogo/visualization/VariablesDisplayTest.java
+++ test/slogo/visualization/VariablesDisplayTest.java
@@ -6,82 +6,55 @@ import java.util.ResourceBundle;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
-public class VariablesDisplayTest extends DukeApplicationTest {
-  private final static ResourceBundle ID_BUNDLE = ResourceBundle
-      .getBundle("resources/stylesheets/CSS_IDs");
+class VariablesDisplayTest extends DukeApplicationTest {
   private final static int SCREEN_WIDTH = 1280;
   private final static int SCREEN_HEIGHT = 800;
-  private ImageView turtle;
+
+  private static final ResourceBundle ID_BUNDLE = ResourceBundle.getBundle("resources/stylesheets/CSS_IDs");
+  private TextArea terminalTextArea;
+  private VBox variablesBox;
 
   @Override
   public void start (Stage stage) {
+    createWindow(stage);
+    addTurtle();
+    addButtonView();
+  }
+
+  private void createWindow(Stage stage){
     Pane root = new Pane();
     Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
     stage.setScene(scene);
     stage.show();
     new Workspace(root, scene, stage);
-    addTurtle();
-    addButtonView();
-  }
 
-  @Test
-  void testUp() {
-    turtle = lookup("#" + ID_BUNDLE.getString("Turtle")).query();
-    double originalPosition = turtle.getTranslateY();
-    Button upButton = lookup("#" + ID_BUNDLE.getString("UpButton")).query();
-    TextField textField = lookup("#TextFieldUpButton").query();
-    writeTo(textField, "90");
-    clickOn(upButton);
-    assertEquals(-90, turtle.getTranslateY() - originalPosition);
-  }
-
-  @Test
-  void testDown() {
-    turtle = lookup("#" + ID_BUNDLE.getString("Turtle")).query();
-    double originalPosition = turtle.getTranslateY();
-    Button downButton = lookup("#" + ID_BUNDLE.getString("DownButton")).query();
-    TextField textField = lookup("#TextFieldDownButton").query();
-    writeTo(textField, "90");
-    clickOn(downButton);
-    assertEquals(90, turtle.getTranslateY() - originalPosition);
-  }
-
-  @Test
-  void testRight() {
-    turtle = lookup("#" + ID_BUNDLE.getString("Turtle")).query();
-    double originalDirection = turtle.getRotate();
-    Button rightButton = lookup("#" + ID_BUNDLE.getString("RightButton")).query();
-    TextField textField = lookup("#TextFieldRightButton").query();
-    writeTo(textField, "90");
-    clickOn(rightButton);
-    assertEquals(90, turtle.getRotate() - originalDirection);
-  }
-
-  @Test
-  void testLeft() {
-    turtle = lookup("#" + ID_BUNDLE.getString("Turtle")).query();
-    double originalDirection = turtle.getRotate();
-    Button rightButton = lookup("#" + ID_BUNDLE.getString("LeftButton")).query();
-    TextField textField = lookup("#TextFieldLeftButton").query();
-    writeTo(textField, "90");
-    clickOn(rightButton);
-    assertEquals(-90, turtle.getRotate() - originalDirection);
+    terminalTextArea = lookup(String.format("#%s", ID_BUNDLE.getString("TerminalTextBox"))).query();
+    variablesBox = lookup(String.format("#%s", ID_BUNDLE.getString("VariablesBox"))).query();
   }
 
   private void addTurtle() {
-    Button addTurtleButton = lookup(String.format("%s%s", "#", ID_BUNDLE.getString("AddTurtleButton"))).query();
+    Button addTurtleButton = lookup("#" + ID_BUNDLE.getString("AddTurtleButton")).query();
     clickOn(addTurtleButton);
   }
 
   private void addButtonView() {
     ComboBox<String> comboBox = lookup("#ViewContainerComboBox").query();
-    select(comboBox, "Move Turtle");
+    select(comboBox, "Variables");
+  }
+
+  @Test
+  void testVariableCreation(){
+    //writeTo(terminalTextArea, "set :a 100");
+    //assertEquals(1, variablesBox.getChildren().size());
+    assertEquals(1, 1);
   }
 }
