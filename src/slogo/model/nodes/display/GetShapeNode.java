72f08ababe0f6d9f28cb1134921a21dcfd1e1c92
Felix Jiang <felix.jiang@duke.edu>
2021-03-22 00:20:45 -0400

    get shape command done

--- src/slogo/model/nodes/display/GetShapeNode.java
+++ src/slogo/model/nodes/display/GetShapeNode.java
@@ -1,2 +1,21 @@
-package slogo.model.nodes.display;public class GetShapeNode {
+package slogo.model.nodes.display;
+
+import java.util.ArrayList;
+import slogo.model.BackEndTurtleTracker;
+import slogo.model.nodes.commands.TurtleCommandNode;
+import slogo.turtlecommands.DisplayCommand;
+
+public class GetShapeNode extends TurtleCommandNode {
+
+  public GetShapeNode(int numParameters) {
+    super(numParameters);
+  }
+
+  @Override
+  public double getReturnValue(BackEndTurtleTracker tracker) {
+    return super.loopThroughTurtles(tracker, new ArrayList<>(), (currTurtle, values) -> {
+      currTurtle.addCommand( new DisplayCommand("Turtle Shape Index is " + currTurtle.getShapeIndex()));
+      return currTurtle.getShapeIndex();
+    });
+  }
 }
