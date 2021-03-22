dafc32251288070df2c448a032d1619557038e6b
Felix Jiang <felix.jiang@duke.edu>
2021-03-22 00:17:00 -0400

    pen color node complete

--- src/slogo/model/nodes/display/GetPenColorNode.java
+++ src/slogo/model/nodes/display/GetPenColorNode.java
@@ -1,2 +1,21 @@
-package slogo.model.nodes.display;public class GetPenColorNode {
+package slogo.model.nodes.display;
+
+import java.util.ArrayList;
+import slogo.model.BackEndTurtleTracker;
+import slogo.model.nodes.commands.TurtleCommandNode;
+import slogo.turtlecommands.DisplayCommand;
+
+public class GetPenColorNode extends TurtleCommandNode {
+
+  public GetPenColorNode(int numParameters) {
+    super(numParameters);
+  }
+
+  @Override
+  public double getReturnValue(BackEndTurtleTracker tracker) {
+    return super.loopThroughTurtles(tracker, new ArrayList<>(), (currTurtle, values) -> {
+      currTurtle.addCommand( new DisplayCommand("Pen Color Index is " + values.get(0).intValue()));
+      return values.get(0);
+    });
+  }
 }
