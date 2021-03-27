9b75eb168b668527f9a62585481719ec3b0dcac3
Felix Jiang <felix.jiang@duke.edu>
2021-03-26 23:38:17 -0400

    finish command node'

--- src/slogo/model/nodes/control/CommandNode.java
+++ src/slogo/model/nodes/control/CommandNode.java
@@ -5,7 +5,20 @@ import java.util.List;
 import slogo.model.SlogoNode;
 import slogo.model.BackEndTurtleTracker;
 
-// represents the actual node when a user calls the command they defined, so return value is not just 1 or 0
+/**
+ * Represents the actual node when a user calls the command they defined, so return value is not
+ * just 1 or 0.
+ *
+ * In a recursive user defined command, each command node will have the same list of variable nodes
+ * and same myCommands but the parameters are different, that those are what determines the exit
+ * condition.
+ *
+ *      return new CommandNode(variableNames.size(), myCommands, variableNames);
+ *      // in MakeUserInstructionNode
+ *
+ * @author Felix Jiang
+ * @author Andre Wang
+ */
 public class CommandNode extends SlogoNode {
 
   private List<SlogoNode> myCommands;
@@ -13,6 +26,14 @@ public class CommandNode extends SlogoNode {
   private List<VariableNode> variables;
   private List<Double> prevValues;
 
+  /**
+   * Constructor for command node. Created in a MakeUserInstructionNode each time the command name
+   * is seen in the program (including in its definition itself if it is recursive).
+   *
+   * @param numParameters number of parameters for this custom node
+   * @param myCommands list of commands that are in this user defined command
+   * @param variables list of variable nodes that are part of this user defined command.
+   */
   public CommandNode(int numParameters, List<SlogoNode> myCommands, List<VariableNode> variables) {
     super(numParameters);
     parameters = super.getParameters();
@@ -21,6 +42,18 @@ public class CommandNode extends SlogoNode {
     prevValues = new ArrayList<>();
   }
 
+  /**
+   * Execute the user defined commands by looping through each node in myCommands.
+   *
+   * Update variable scope by saving variable values before execution and replacing them after.
+   *
+   * Calculate parameter values and set those to the variable nodes before command is run.
+   *
+   * @param tracker keeps track of all the turtles, allows commands that require receiving turtle
+   *                information or adding commands to a turtle to do so with the parameter, rather
+   *                than an instance variable present in every subclass.
+   * @return value of the last executed command in the user defined command.
+   */
   @Override
   public double getReturnValue(BackEndTurtleTracker tracker) {
     double ret = 0;
