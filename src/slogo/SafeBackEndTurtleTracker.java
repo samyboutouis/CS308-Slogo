11350ee8c767a72f6501535190722583211739b2
Felix Jiang <felix.jiang@duke.edu>
2021-03-26 22:13:16 -0400

    safe back end tracker

--- src/slogo/SafeBackEndTurtleTracker.java
+++ src/slogo/SafeBackEndTurtleTracker.java
@@ -3,8 +3,23 @@ package slogo;
 import java.util.List;
 import java.util.Map;
 
-// what backend passes to front end, limiting front end from other backend turtle tracker methods
+/**
+ * What backend passes to front end, limiting front end from other backend turtle tracker methods.
+ *
+ *    controller.parseProgram(command, backEndTurtleTracker).getAllTurtleCommands()
+ *
+ * parseProgram() returns a SafeBackEndTurtleTracker, and then it calls the single method of this
+ * interface.
+ *
+ * @author Felix Jiang
+ */
 public interface SafeBackEndTurtleTracker {
 
-  Map<Integer, List<Command>> getAllTurtleCommands(); // for front end to get map
+  /**
+   * For front end to receive map of turtle ID and the list of commands it should run on that
+   * turtle.
+   *
+   * @return map of turtle ID to list of commands.
+   */
+  Map<Integer, List<Command>> getAllTurtleCommands();
 }
