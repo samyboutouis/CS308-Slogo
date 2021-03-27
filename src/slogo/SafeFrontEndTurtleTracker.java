fe00939b6ce2ff6780698fbc014afffcf6b5ef72
Felix Jiang <felix.jiang@duke.edu>
2021-03-26 22:10:46 -0400

    finish safe front end tracker

--- src/slogo/SafeFrontEndTurtleTracker.java
+++ src/slogo/SafeFrontEndTurtleTracker.java
@@ -1,14 +1,49 @@
 package slogo;
 
-// what backend has access to and can pass to each Command object for the command to run
+/**
+ * Interface for backend to have access to in order for it to pass this type to each Command object
+ * for the command to run.
+ *
+ * Defines the methods that backend can call on the front end turtle tracker.
+ *
+ *      new SetBackgroundCommand(tracker.getSafe(), values.get(0).intValue())
+ *
+ * Pass in this type with tracker.getSafe(), which can be used by the setbg command once it is ran.
+ *
+ * @author Felix Jiang
+ * @author Samy Boutouis
+ * @author Donghan Park
+ */
 public interface SafeFrontEndTurtleTracker {
 
-  // defines the methods that backend turtle tracker can call on the front end turtle
+  /**
+   * Set turtle active with this id.
+   *
+   * @param id id of turtle to be set active.
+   */
   void setActive(int id);
 
+  /**
+   * Set turtle inactive with this id.
+   *
+   * @param id id of turtle to be set inactive.
+   */
   void setInactive(int id);
 
+  /**
+   * Set background color to the index of the palette.
+   *
+   * @param index index of palette to set the background color to.
+   */
   void setBackgroundColor(int index);
 
+  /**
+   * Add or update an entry on the palette.
+   *
+   * @param index index to add or update the palette
+   * @param r amount of red for this new palette entry.
+   * @param g amount of green for this new palette entry.
+   * @param b amount of blue for this new palette entry.
+   */
   void updatePalette(int index, int r, int g, int b);
 }
