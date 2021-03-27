f45bb01ba5dc176610aca98aa12ea79f08d366a5
Felix Jiang <felix.jiang@duke.edu>
2021-03-26 22:47:45 -0400

    backward node

--- src/slogo/model/nodes/commands/BackwardNode.java
+++ src/slogo/model/nodes/commands/BackwardNode.java
@@ -5,16 +5,33 @@ import slogo.model.BackEndTurtleTracker;
 import slogo.turtlecommands.MovementCommand;
 import slogo.model.SlogoNode;
 
+/**
+ * Represents the node of the SlogoNode tree for a backward command.
+ *
+ * @author Felix Jiang
+ * @author Andre Wang
+ */
 public class BackwardNode extends TurtleCommandNode {
 
   private List<SlogoNode> parameters;
-  private List<Double> values;
 
+  /**
+   * Constructor for node.
+   *
+   * @param numParameters number of parameters a backward node takes (1)
+   */
   public BackwardNode(int numParameters) {
     super(numParameters);
     parameters = super.getParameters();
   }
 
+  /**
+   *
+   * @param tracker keeps track of all the turtles, allows commands that require receiving turtle
+   *                information or adding commands to a turtle to do so with the parameter, rather
+   *                than an instance variable present in every subclass.
+   * @return output value of going backwards (just the parameter)
+   */
   @Override
   public double getReturnValue(BackEndTurtleTracker tracker) {
     return super.loopThroughTurtles(tracker, parameters, (currTurtle, values) -> {
